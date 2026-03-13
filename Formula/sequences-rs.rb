fb47d84bab5e521d0fbb5ba90cad440014905e0b
Divkov575 <dmitriy@ivkov.net>
2026-03-13 00:06:01 -0400

    sequences-rs 0.2.2

--- Formula/sequences-rs.rb
+++ Formula/sequences-rs.rb
@@ -1,7 +1,7 @@
 class SequencesRs < Formula
   desc "CLI for training number sequence recognition (Optiver/Flow Traders prep)"
   homepage "https://github.com/DIvkov575/sequences-rs"
-  url "https://github.com/DIvkov575/sequences-rs/archive/refs/tags/v0.2.3.tar.gz"
+  url "https://github.com/DIvkov575/sequences-rs/archive/refs/tags/v0.2.2.tar.gz"
   sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "MIT"
 
